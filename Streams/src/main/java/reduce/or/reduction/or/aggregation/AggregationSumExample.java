df4791eff2f2c666c8091897ecc9f32db95c287d
AMIT GARG <amit.a.garg@ericsson.com>
2018-11-17 11:43:19 +0530

    Stream: Aggregation Sum Example

---
+++ Streams/src/main/java/reduce/or/reduction/or/aggregation/AggregationSumExample.java
@@ -0,0 +1,38 @@
+package reduce.or.reduction.or.aggregation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.stream.Stream;
+
+public class AggregationSumExample {
+
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1, 2, 3, 4);
+		
+		Stream<Integer> stream = list.stream();
+		
+		Integer identity = 0;
+		
+		BinaryOperator<Integer> sumLogic = (arg1, arg2) -> arg1 + arg2;
+		
+		Integer sum = stream.reduce(identity, sumLogic);
+		
+		System.out.println(sum);   // Output : 10
+		
+		stream = Stream.empty();
+		
+		System.out.println(stream.reduce(identity, sumLogic));    // Output: 0
+		
+		stream = Stream.of(2);
+		
+		System.out.println(stream.reduce(identity, sumLogic));    // Output: 2
+		
+		stream = Stream.of(-20);
+		
+		System.out.println(stream.reduce(identity, sumLogic));    // Output: -20	
+		
+		
+	}
+	
+}
