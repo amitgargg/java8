b743401a7c8b8557e7d91aa4f01cd608ad0d6554
AMIT GARG <amit.a.garg@ericsson.com>
2018-11-17 14:18:37 +0530

    Stream: Get Person Name in Comma Separated Order

---
+++ Streams/src/main/java/collectors/CollectorGroupByDownStreamByPersonNameCommaSperated.java
@@ -0,0 +1,45 @@
+package collectors;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import optional.Person;
+
+public class CollectorGroupByDownStreamByPersonNameCommaSperated {
+
+	public static void main(String[] args) {
+		List<Person> persons = getPersons();
+
+		Function<Person, String> mapper = (person) -> person.getName();
+		Collector<CharSequence, ?, String> downstream = Collectors.joining(", ");
+		Collector<Person, ?, String> personNameCollector 
+										= Collectors.mapping(mapper, downstream);
+
+		Function<Person, Integer> collectFunction = (person) -> person.getAge();
+		Collector<Person, ?, Map<Integer, String>> collector 
+				= Collectors.groupingBy(collectFunction, personNameCollector);
+
+		Map<Integer, String> personmap = persons.stream().collect(collector);
+
+		System.out.println(personmap);
+	}
+
+	
+	private static List<Person> getPersons() {
+		Person p1 = new Person("AMIT", 100);
+		Person p2 = new Person("SUMIT", 100);
+		Person p3 = new Person("RAM", 90);
+		Person p4 = new Person("LXMAN", 30);
+		Person p5 = new Person("POOJA", 20);
+		Person p6 = new Person("KAPIL", 80);
+		Person p7 = new Person("SHIVANI", 60);
+		Person p8 = new Person("KHUSHI", 10);
+		Person p9 = new Person("SHARDA", 70);
+		Person p10 = new Person("VIPIN", 40);
+		return Arrays.asList(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10);
+	}
+}
